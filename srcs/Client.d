e360eb691108f0bcbfa484729a5f7cb318ee3abb
gbertin12 <gaelbertin12@gmail.com>
2023-03-08 09:04:29 +0100

    advance

--- srcs/Client.d
+++
@@ -1,6 +0,0 @@
-srcs/Client.o: srcs/Client.cpp srcs/../headers/Client.hpp \
-  srcs/../headers/Channel.hpp srcs/../headers/modes/ChannelModes.hpp \
-  srcs/../headers/modes/../Client.hpp srcs/../headers/Command.hpp \
-  srcs/../headers/Server.hpp srcs/../headers/Utils.h \
-  srcs/../headers/modes/UserModes.hpp \
-  srcs/../headers/modes/PrivilegesModes.hpp
